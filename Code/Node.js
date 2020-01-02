60601ab4028e329e26f5f5c45b36505a406e5a69
LaNeraNura <50608975+LaNeraNura@users.noreply.github.com>
2020-01-02 13:07:32 +0800

    Payments in Node.js
    Payments

---
+++ Code/Node.js
@@ -0,0 +1,56 @@
+// Enter Client Credentials
+const environment = 'https://openapi-test.qfpay.com'
+const app_code = 'D5589D2A1F2E42A9A60C37E322E64454'
+const client_key = '0E32A59A8B454940A2FF39D6DD05FF1C'
+
+// Generate Timestamp
+var dateTime = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
+console.log(dateTime)
+
+// Body Payload
+const key = client_key
+var tradenumber = String(Math.round(Math.random() * 1000000000))
+console.log(tradenumber)
+
+var payload = {
+'txamt': '10', // In USD,EUR,etc. Cent
+'txcurrcd': 'EUR',
+'pay_type': '800101', // Alipay CPM = 800108 , MPM = 800101
+'out_trade_no': tradenumber,
+'txdtm': dateTime,
+'mchid': 'ZaMVgIlxkg'
+};
+
+// Signature Generation
+const ordered = {};
+Object.keys(payload).sort().forEach(function(key) {
+  ordered[key] = payload[key] });
+console.log(ordered)
+
+var str = [];
+for (var p in ordered)
+if (ordered.hasOwnProperty(p)) {
+str.push((p) + "=" + (ordered[p]));
+}
+var string = str.join("&")+client_key;
+console.log(string)
+
+const crypto = require('crypto')
+var hashed = crypto.createHash('md5').update(string).digest('hex')
+console.log(hashed)
+
+
+// API Request
+var request = require("request");
+request({
+  uri: environment+"/trade/v1/payment",
+  headers: {
+    'X-QF-APPCODE': app_code,
+    'X-QF-SIGN': hashed
+  },
+  method: "POST",
+  form: payload,
+  }, 
+  function(error, response, body) {
+  console.log(body);
+});
